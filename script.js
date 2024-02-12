ca8fd996875f9bddbeeed861cd2352df20a1e364
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-12 23:19:05 +0400

    add javascript manipulations

---
+++ script.js
@@ -0,0 +1,23 @@
+let divs = document.getElementsByClassName("question-div");
+let arrows = document.getElementsByClassName("arrow-down");
+let answers = document.getElementsByClassName("answer");
+let question = document.getElementsByClassName("question");
+let down = true;
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener("click", function () {
+    if (down) {
+      arrows[i].style.transform = "rotate(180deg)";
+      answers[i].style.display = "block";
+      question[i].style.color = "#1e1f36";
+      question[i].style.fontWeight = "bold";
+      down = false;
+    } else {
+      arrows[i].style.transform = "rotate(0deg)";
+      answers[i].style.display = "none";
+      question[i].style.color = "#4b4c5f";
+      question[i].style.fontWeight = "normal";
+      down = true;
+    }
+  });
+}
