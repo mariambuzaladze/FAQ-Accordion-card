06ce589777ea37687ec56d729d34928060a58d36
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-14 22:15:32 +0400

    add new functionality

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ let answers = document.getElementsByClassName("answer");
 let question = document.getElementsByClassName("question");
 let down = true;
 
+let clicked;
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener("click", function () {
     if (down) {
+      if (clicked === undefined) {
+      } else {
+        arrows[clicked].style.transform = "rotate(0deg)";
+        answers[clicked].style.display = "none";
+        question[clicked].style.color = "#4b4c5f";
+        question[clicked].style.fontWeight = "normal";
+        clicked = i;
+      }
       arrows[i].style.transform = "rotate(180deg)";
       answers[i].style.display = "block";
       question[i].style.color = "#1e1f36";
       question[i].style.fontWeight = "bold";
       down = false;
+      clicked = i;
     } else {
       arrows[i].style.transform = "rotate(0deg)";
       answers[i].style.display = "none";
