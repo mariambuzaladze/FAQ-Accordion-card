321239a6e9d15fa20f07d7f667cb0e6e1187f27d
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-16 00:37:34 +0400

    update javascript functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let divs = document.getElementsByClassName("question-div");
-let arrowDown = true;
 let clicked;
 
 for (let i = 0; i < divs.length; i++) {
@@ -8,41 +7,12 @@ for (let i = 0; i < divs.length; i++) {
       divs[clicked].classList.remove("active");
     }
 
-    if (arrowDown) {
-      divs[i].classList.add("active");
-      clicked = i;
-    } else {
+    if (clicked === i) {
       divs[i].classList.remove("active");
       clicked = undefined;
+    } else {
+      divs[i].classList.add("active");
+      clicked = i;
     }
-
-    arrowDown = !arrowDown;
   });
 }
-// for (let i = 0; i < divs.length; i++) {
-//   divs[i].addEventListener("click", function () {
-//     if (down) {
-//       if (clicked === undefined) {
-//       } else {
-//         arrows[clicked].style.transform = "rotate(0deg)";
-//         answers[clicked].style.display = "none";
-//         question[clicked].style.color = "#4b4c5f";
-//         question[clicked].style.fontWeight = "normal";
-//         // clicked = undefined;
-//       }
-//       arrows[i].style.transform = "rotate(180deg)";
-//       answers[i].style.display = "block";
-//       question[i].style.color = "#1e1f36";
-//       question[i].style.fontWeight = "bold";
-//       down = false;
-//       clicked = i;
-//     } else {
-//       arrows[i].style.transform = "rotate(0deg)";
-//       answers[i].style.display = "none";
-//       question[i].style.color = "#4b4c5f";
-//       question[i].style.fontWeight = "normal";
-//       down = true;
-//       // clicked = undefined;
-//     }
-//   });
-// }
